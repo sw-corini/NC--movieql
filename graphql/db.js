bc59a545b43f4a6e4de9e0da01cbd21001e93e1b
sw <genie_s2@naver.com>
2019-02-13 17:30:52 +0900

    #10 Delete Mutation

--- graphql/db.js
+++ graphql/db.js
@@ -1,4 +1,4 @@
-const movies = [
+let movies = [
     {
         id: 0,
         name: "Star Wars - The new one",
@@ -26,7 +26,7 @@ export const getById = id => {
 export const deleteMovie = id => {
     const cleanedMovies = movies.filter(movie => movie.id !== id);
     if (movies.length > cleanedMovies.length) {
-        movie = cleanMovies;
+        movies = cleanedMovies;
         return true;
     } else {
         return false;
