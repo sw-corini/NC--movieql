c9eef978be5557ebb230063bc601b098c001fc67
sw <genie_s2@naver.com>
2019-02-14 09:40:07 +0900

    #12 Wrapping a REST API with GraphQL Part Two

--- graphql/db.js
+++ graphql/db.js
@@ -1,54 +1,17 @@
 import fetch from "node-fetch";
 
-const API_URL = "https://yts.am/api/v2/list_movies.json";
+const API_URL = "https://yts.am/api/v2/list_movies.json?";
 
 export const getMovies = (limit, rating) => {
-    fetch(`${API_URL}`)
+    let REQUEST_URL = API_URL;
+    if (limit > 0) {
+        REQUEST_URL += `limit=${limit}`;
+    }
+    if (rating) {
+        REQUEST_URL += `&minimum_rating=${rating}`;
+    }
+
+    return fetch(REQUEST_URL)
         .then(res => res.json())
         .then(json => json.data.movies);
 };
-
-// let movies = [
-//     {
-//         id: 0,
-//         name: "Star Wars - The new one",
-//         score: 1
-//     },
-//     {
-//         id: 1,
-//         name: "Avengers - The new one",
-//         score: 8
-//     },
-//     {
-//         id: 2,
-//         name: "The Godfather I",
-//         score: 99
-//     }
-// ];
-
-// export const getMovies = () => movies;
-
-// export const getById = id => {
-//     const filteredMovies = movies.filter(movie => movie.id === id);
-//     return filteredMovies[0];
-// };
-
-// export const deleteMovie = id => {
-//     const cleanedMovies = movies.filter(movie => movie.id !== id);
-//     if (movies.length > cleanedMovies.length) {
-//         movies = cleanedMovies;
-//         return true;
-//     } else {
-//         return false;
-//     }
-// };
-
-// export const addMovie = (name, score) => {
-//     const newMovie = {
-//         id: movies.length + 1,
-//         name,
-//         score
-//     };
-//     movies.push(newMovie);
-//     return newMovie;
-// };
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,7 +1,7 @@
 import { getMovies } from "./db";
 const resolvers = {
     Query: {
-        movies: () => getMovies()
+        movies: (_, { limit, rating }) => getMovies(limit, rating)
     }
 };
 export default resolvers;
